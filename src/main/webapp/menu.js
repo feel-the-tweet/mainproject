07ee2c7b5ea9b6486dc5996025f889e16dcb4029
Agustín Núñez Arenas <Agus@champinon.local>
2017-03-23 00:35:46 +0100

    Completada la web del primer entregable
    Se ha completado la web que se usará para el primer entregable, además se ha corregido el redimensionamiento de la página.

---
+++ src/main/webapp/menu.js
@@ -0,0 +1,30 @@
+/* Ajusta la altura ("height") para que el menú se alargue hasta el final de la página */
+function adjustMenu() {
+    /* Debug code
+    document.getElementById("logg").innerHTML = document.getElementById("contenido").scrollHeight + "px";
+    document.getElementById("logg1").innerHTML = window.innerHeight + "px";
+    document.getElementById("logg2").innerHTML = document.getElementById("footer").clientHeight + "px";
+    document.getElementById("logg3").innerHTML = window.innerWidth + "px";
+    */
+    
+    if (window.innerWidth > 767) {
+        if (window.innerHeight > document.getElementById("contenido").scrollHeight + document.getElementById("footer").clientHeight) {
+            document.getElementById("row content").style.height = window.innerHeight - document.getElementById("footer").clientHeight + "px";
+        } else {
+            document.getElementById("row content").style.height = document.getElementById("contenido").scrollHeight + "px";
+        }
+    } else {
+        if (window.innerHeight > document.getElementById("row content").scrollHeight + document.getElementById("footer").clientHeight) {
+            document.getElementById("row content").style.height = window.innerHeight - document.getElementById("footer").clientHeight + "px";
+        } else {
+            document.getElementById("row content").style.height = document.getElementById("row content").scrollHeight + "px";
+        }
+    }
+
+}
+
+
+/* Esta función se llama cada vez que se redimensiona la página */
+window.onresize = function (event) {
+    adjustMenu();
+};
